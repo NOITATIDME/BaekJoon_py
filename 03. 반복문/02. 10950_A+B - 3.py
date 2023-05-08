b052d218197c21eb5f060b59902255523c95c0a7
NOITATIDME <chgt186@naver.com>
2023-05-08 12:31:23 +0900

    [반복문] 02번, 03번

--- 03. 반복문/02. 10950_A+B - 3.py
+++ 03. 반복문/02. 10950_A+B - 3.py
@@ -1,1 +1,5 @@
-A, B = map(int,input().split())
+T = int(input())
+
+for i in range(T):
+    A, B = map(int,input().split())
+    print(A + B)
---
+++ 03. 반복문/03. 8393_합.py
@@ -0,0 +1,7 @@
+A = int(input())
+B = 0
+
+for i in range(A+1):
+    B = B + i
+
+print(B)
