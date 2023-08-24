eb99a5e7d4c8da0ae4cc3451c7952bc9143e7615
NOITATIDME <chgt186@naver.com>
2023-08-25 00:45:48 +0900

    [반복문] 04번

--- 03. 반복문/04. 25304_영수증.py
+++ 03. 반복문/04. 25304_영수증.py
@@ -1,22 +1,8 @@
-##첫째 줄에는 영수증에 적힌 총 금액 X가 주어진다.
-X = int(input())
-## 둘째 줄에는 영수증에 적힌 구매한 물건의 종류의 수 N 이 주어진다.
-N = int(input())
-## 이후 N개의 줄에는 각 물건의 가격 a와 개수 b가 공백을 사이에 두고 주어진다.
-total = 0
-cost = 0
-print(N)
-for i in range(N+1):
+x = int(input())
+sum = 0
+
+for _ in range(int(input())):
     a, b = map(int, input().split())
-    cost = a * b
-    total += cost
+    sum += a * b
 
-if(total == X):
-    print('YES')
-
-#else if(total != X):
-#   print('NO')
-#else:
-
-#    print(total)
-#    print(i)
+print("Yes") if sum == x else print("No")
