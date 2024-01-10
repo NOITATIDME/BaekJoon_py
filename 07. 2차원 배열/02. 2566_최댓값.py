0e0ed12b2a1acc6ef664830008c1eaca6038fad5
NOITATIDME <chgt186@naver.com>
2024-01-10 17:13:47 +0900

    [2차원 배열] 02, 03번

--- 07. 2차원 배열/02. 2566_최댓값.py
+++ 07. 2차원 배열/02. 2566_최댓값.py
@@ -1,10 +1,13 @@
-A = []
-for i in range(9):
-    a = list(map(int, input().split()))
-    A.append(a)
+table = [list(map(int, input().split())) for _ in range(9)]
 
-max_value = max(map(max, A))
+max_num = 0
+max_row, max_col = 0, 0
 
-index = 0
-for i in A:
-    i.find(max_value)
+for row in range(9):
+    for col in range(9):
+        if max_num <= table[row][col]:
+            max_row = row + 1
+            max_col = col + 1
+            max_num = table[row][col]
+print(max_num)
+print(max_row, max_col)
---
+++ 07. 2차원 배열/03. 10798_세로읽기.py
@@ -0,0 +1,8 @@
+table = [list(map(str , input().split())) for _ in range(5)]
+
+print(table)
+
+# for row in range(5):
+#     print('len(table[row] :  ',len(table[row]))
+#     for col in range(len(table[row])):
+#         print(table[col][row], end='')
