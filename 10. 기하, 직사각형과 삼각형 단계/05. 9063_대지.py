27cad905407b12e57b18df93093009296518085f
NOITATIDME <chgt186@naver.com>
2024-01-26 17:40:34 +0900

    [기하, 직사각형과 삼각형 단계] 05번

--- 10. 기하, 직사각형과 삼각형 단계/05. 9063_대지.py
+++ 10. 기하, 직사각형과 삼각형 단계/05. 9063_대지.py
@@ -1,5 +1,15 @@
 N = int(input())
 
+xList = []
+yList = []
+
 for i in range(N):
     x, y = map(int, input().split())
+    xList.append(x)
+    yList.append(y)
 
+
+if N == 1:
+    print(0)
+else:
+    print((max(xList) - min(xList)) * (max(yList) - min(yList)))
